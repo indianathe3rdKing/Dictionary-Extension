6e9f214a1c1cbf0dbe8db594a75246c25414f641
indianathe3rd <indiemenziwa@gmail.com>
2025-09-20 16:19:48 +0200

    dsiplay definitions dynamically after search

--- src/components/search-accordion.jsx
+++ src/components/search-accordion.jsx
@@ -13,7 +13,7 @@ export default function SearchAccordion({ result }) {
       id: "1",
       title: (
         <h2 className="uppercase text-2xl font-bold">
-          {result?.word || "Loading..."}
+          {result?.word || "Definition"}
         </h2>
       ),
       content: (
@@ -78,25 +78,27 @@ export default function SearchAccordion({ result }) {
   ];
   return (
     <div className="space-y-4">
-      <Accordion
-        type="single"
-        collapsible
-        className="w-[90%] m-auto"
-        defaultValue="1"
-      >
-        {items.map((item) => (
-          <AccordionItem value={item.id} key={item.id} className="py-2">
-            <AccordionPrimitive.Header className="flex">
-              <AccordionPrimitive.Trigger className="focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-center justify-between gap-4 rounded-md py-2 text-left text-sm text-[15px] leading-6 font-semibold transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&>svg>path:last-child]:origin-center [&>svg>path:last-child]:transition-all [&>svg>path:last-child]:duration-200 [&[data-state=open]>svg]:rotate-180 [&[data-state=open]>svg>path:last-child]:rotate-90 [&[data-state=open]>svg>path:last-child]:opacity-0">
-                {item.title}
-              </AccordionPrimitive.Trigger>
-            </AccordionPrimitive.Header>
-            <AccordionContent className="text-muted-foreground pb-2">
-              {item.content}
-            </AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
+      {result && (
+        <Accordion
+          type="single"
+          collapsible
+          className="w-[90%] m-auto"
+          defaultValue="1"
+        >
+          {items.map((item) => (
+            <AccordionItem value={item.id} key={item.id} className="py-2">
+              <AccordionPrimitive.Header className="flex">
+                <AccordionPrimitive.Trigger className="focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-center justify-between gap-4 rounded-md py-2 text-left text-sm text-[15px] leading-6 font-semibold transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&>svg>path:last-child]:origin-center [&>svg>path:last-child]:transition-all [&>svg>path:last-child]:duration-200 [&[data-state=open]>svg]:rotate-180 [&[data-state=open]>svg>path:last-child]:rotate-90 [&[data-state=open]>svg>path:last-child]:opacity-0">
+                  {item.title}
+                </AccordionPrimitive.Trigger>
+              </AccordionPrimitive.Header>
+              <AccordionContent className="text-muted-foreground pb-2">
+                {item.content}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      )}
     </div>
   );
 }
